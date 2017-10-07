81bb0a20404334a808599754e0ea1661655b065a
khomesh24 <khomeshthakre24@gmail.com>
2017-10-07 22:43:29 +0530

    added ubuntu setup for libvirt kvm and log are generated.

--- libvirt.sh
+++ libvirt.sh
@@ -6,14 +6,18 @@ log_file=lvinstall.log
 #function installation for ubuntu
 function for_ubuntu {
 	echo $1
+	apt-get install kvm -y &>> $log_file
+	apt-get install qemu-kvm libvirt-bin -y &>> $log_file
+	apt-get install virtinst -y &>> $log_file
+
 }
 
 #function installation for fedora
 function for_fedora { 
 	echo $1
-	dnf install kvm -y $>> $log_file
+	dnf install kvm -y &>> $log_file
 	dnf install qemu-kvm python-virtinst libvirt libvirt-python virt-manager libguestfs-tools -y &>> $log_file
-}
+} 
 
 
 distro=$(lsb_release -i | cut -f 2)
