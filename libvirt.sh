f16ce29c216869bfd68c1728c7ba778ecb90ce76
khomesh24 <khomeshthakre24@gmail.com>
2017-10-09 08:00:39 +0530

    some correction is done. Now this script check whether package is installed or not and then installed.

--- libvirt.sh
+++ libvirt.sh
@@ -3,27 +3,46 @@
 
 log_file=lvinstall.log
 
+function check {
+	package=$1
+	result=$(dnf -q list installed libvirt &>/dev/null && echo "Installed" || echo "Not installed")
+	if [ "$result" == "Installed" ]
+		then
+			#echo "if"
+			echo "$package already installed."
+	elif [ "$result" == "Not installed" ]
+		then
+			echo "$package is installing"
+			dnf install $package -y
+		fi	
+}
+
+
 #function installation for ubuntu
 function for_ubuntu {
 	echo $1
 	apt-get install kvm -y &>> $log_file
 	apt-get install qemu-kvm libvirt-bin -y &>> $log_file
 	apt-get install virtinst -y &>> $log_file
-
+ 	apt-get install virt-manager -y &>> $log_file
 }
+
+
 
 #function installation for fedora
 function for_fedora { 
-	echo $1
-	dnf install kvm -y &>> $log_file
-	dnf install qemu-kvm python-virtinst libvirt libvirt-python virt-manager libguestfs-tools -y &>> $log_file
+	#echo $1
+	
+	check libvirt
+
 } 
 
 
 distro=$(lsb_release -i | cut -f 2)
-if [ $distro == "Fedora" ]
+if [ "$distro" == "Fedora" ]
 	then
 	for_fedora hello_fedora
-elfi [ $distro == "Ubuntu" ]
+elif [ "$distro" == "Ubuntu" ]
 	then 
 	for_ubuntu hello_ubuntu
+fi
